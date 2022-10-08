1f3bf17126d5dc80373c4c85431ae20a5c38db7d
Kreedzt <zhaozisong1@live.com>
2022-10-08 17:53:19 +0800

    :wrench: Kreedzt - add prettier

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,47 @@ const env = process.env as {
     PUBLIC_KEY: string;
 };
 
-const { Client, GatewayIntentBits, REST, SlashCommandBuilder, Routes } = discord;
+const { Client, GatewayIntentBits, REST, SlashCommandBuilder, Routes } =
+    discord;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
-
 
 client.login(env.DISCORD_TOKEN);
 
 console.log('> Discord Bot started.');
 
-
 // commands
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('servers').setDescription('Replies with all server status!'),
-].map(command => command.toJSON());
+    new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with pong!'),
+    new SlashCommandBuilder()
+        .setName('servers')
+        .setDescription('Replies with all server status!'),
+].map((command) => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(env.APP_ID, env.GUILD_ID), { body: commands })
-.then((data: any) => console.log(`Successfully registered ${data.length} application commands.`))
-.catch(console.error);
+rest.put(Routes.applicationGuildCommands(env.APP_ID, env.GUILD_ID), {
+    body: commands,
+})
+    .then((data: any) =>
+        console.log(
+            `Successfully registered ${data.length} application commands.`
+        )
+    )
+    .catch(console.error);
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  } else if (interaction.commandName === 'servers') {
-    await interaction.reply({ content: 'server list', ephemeral: true });
-  }
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (interaction.commandName === 'servers') {
+        await interaction.reply({ content: 'server list', ephemeral: true });
+    }
 });
-
