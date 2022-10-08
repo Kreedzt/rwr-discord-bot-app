325d1e9ef30ce64297861437cd820e97bce3b882
root <root@C20220930148592.local>
2022-10-08 09:31:53 +0000

    :sparkles: Kreedzt - add slash commands response

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const env = process.env as {
     PUBLIC_KEY: string;
 };
 
-const { Client, GatewayIntentBits  } = discord;
+const { Client, GatewayIntentBits, REST, SlashCommandBuilder, Routes } = discord;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -17,14 +17,31 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+client.login(env.DISCORD_TOKEN);
+
+console.log('> Discord Bot started.');
+
+
+// commands
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('servers').setDescription('Replies with all server status!'),
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(env.DISCORD_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(env.APP_ID, env.GUILD_ID), { body: commands })
+.then((data: any) => console.log(`Successfully registered ${data.length} application commands.`))
+.catch(console.error);
+
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
+  } else if (interaction.commandName === 'servers') {
+    await interaction.reply({ content: 'server list', ephemeral: true });
   }
 });
 
-client.login(env.DISCORD_TOKEN);
-
-console.log('> Discord Bot started.');
